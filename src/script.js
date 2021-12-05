05c190dfc0a5410bae8a76da65adf8e04ce21c9b
ishayasolo <ishaya808@gmail.com>
2021-12-05 01:36:12 +0100

    modify code to be mobile friendly

---
+++ src/script.js
@@ -0,0 +1,13 @@
+// console.log('script.js is running...');
+
+function openNav() {
+  let navContent = document.getElementById('navContent');
+  
+  navContent.style.width = '100%';
+}
+
+function closeNav() {
+  let navContent = document.getElementById('navContent');
+  
+  navContent.style.width = '0';
+}
