02891b05e344367aca43a33815343c4a1d925de5
ishayasolo <ishaya808@gmail.com>
2021-12-19 09:12:09 +0100

    finish header and fix minor bugs in the nav section

--- src/script.js
+++ src/script.js
@@ -2,12 +2,16 @@
 
 function openNav() {
   let navContent = document.getElementById('navContent');
-  
+  let body = document.getElementById('body');
+
   navContent.style.width = '100%';
+  body.style.overflow = 'hidden';
 }
 
 function closeNav() {
   let navContent = document.getElementById('navContent');
-  
+  let body = document.getElementById('body');
+
   navContent.style.width = '0';
+  body.style.overflow = 'auto';
 }
